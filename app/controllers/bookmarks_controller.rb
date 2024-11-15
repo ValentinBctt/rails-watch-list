8cd7a150f778af5340552092d1528b4517d524a9
valentin bracchetti <valentin.bctt@gmail.com>
2024-11-15 14:46:05 +0100

    en cours

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,23 +1,23 @@
-# app/controllers/bookmarks_controller.rb
 class BookmarksController < ApplicationController
-    def show
-      @bookmark = Bookmark.all
-    end
-    def new
-      @list = List.find(params[:list_id])
-      @bookmark = Bookmark.new
-    end
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
 
-    def create
-      @bookmark = @list.bookmarks.new(bookmark_params)
-      @bookmark.save
-        redirect_to @list, notice: 'Bookmark was successfully created.'
+  def create
+    @list = List.find(params[:list_id])  # Trouve la liste associée
+    @bookmark = @list.bookmarks.new(bookmark_params)  # Crée un nouveau bookmark pour cette liste
+
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark was successfully created.'
+    else
+      render :new
     end
+  end
 
   private
 
-
   def bookmark_params
-    params.require(:bookmark).permit(:movie_id, :comment)
+    params.require(:bookmark).permit(:movie_id, :comment)  # Assure-toi que le paramètre :movie_id est autorisé
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lists do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create, :destroy]
   end
-  # Autres routes...
+
 end
