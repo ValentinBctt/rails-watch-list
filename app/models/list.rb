5bf4381aef250a4d756b7892af2ad06c8c0ea014
valentin bracchetti <valentin.bctt@gmail.com>
2024-11-15 15:37:26 +0100

    en cours

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,9 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy  # Supprime tous les signets associés à la liste
   has_many :movies, through: :bookmarks     # Une liste a plusieurs films via les signets
   has_and_belongs_to_many :movies
+
+    has_one_attached :image
   
+
   validates :name, presence: true, uniqueness: true
 end
